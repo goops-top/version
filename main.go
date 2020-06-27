345ec6f7235101f64cd177c9864e8354c0d84a52
xuxuebiao <xuxuebiao@soulapp.cn>
2020-06-27 13:33:48 +0800

    update go project version

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 	"os"
@@ -8,11 +9,11 @@ import (
 
 func main() {
 
-	args := os.Args 
+	args := os.Args
 	if len(args) >= 2 && args[1] == "version" {
 		v := utils.GetVersion()
 		fmt.Printf("Version: %s\nGitBranch: %s\nCommitId: %s\nBuild Date: %s\nGo Version: %s\nOS/Arch: %s\n", v.Version, v.GitBranch, v.GitCommit, v.BuildDate, v.GoVersion, v.Platform)
 	} else {
-		fmt.Printf("Version(hard code): %s\n","0.1")
+		fmt.Printf("Version(hard code): %s\n", "0.1")
 	}
 }
